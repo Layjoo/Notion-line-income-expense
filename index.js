1ff362acc0cdc11a5247b9c574b6a8900f7085c2
Layjoo <layjoo.jay@gmail.com>
2023-03-30 00:51:51 +0700

    verion 2: line-accouting

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-require("dotenv").config();
-const {
-  sendSelectTags,
-  message
-} = require("./line-object");
-const {
-  getAllTag,
-  getAllItems,
-  addItem,
-  updateItem,
-  getTodayItems,
-  extractNetvaule,
-  todayExpense,
-} = require("./notion")
-const line = require("@line/bot-sdk");
-const app = require('express')();
+import * as dotenv from 'dotenv'
+dotenv.config()
+import { addToList, addUserDocument, filterDataByDateAndUserId, verifyUser } from "./firestore-api.js";
+import { todayAccoutingList } from "./message-object.js";
+import moment from "moment";
+
+
+import line from "@line/bot-sdk";
+import express from 'express'
 const port = process.env.PORT || 3000;
 
-//time setting
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-dayjs.extend(utc)
-dayjs.extend(timezone)
-const today = dayjs().tz("Asia/Bangkok").format("YYYY-MM-DD").toString();
+const app = express();
 
 //setting config for line client
 const config = {
@@ -51,102 +38,104 @@ app.get('/waking', async (req, res) => {
 
 //event handler if user interaction with bot
 async function handleEvent(event) {
-  if (event.type == 'message') {
-    //add new income/expense ex. "-200 ค่าข้าว"
-    if (/[\+\-]/.test(event.message.text)) {
-      console.log("Message >>> add new item");
-      const price = event.message.text.match(/^-\d+|(?<=^\+)\d+/);
-      const detail = event.message.text.match(/(?<=\d\s).*/);
+  const eventType = event.type;
+  console.log(event);
 
-      //add new item
-      const addItemConfig = {
-        detail: detail == null ? undefined : detail[0],
-        income_expense: price == null ? undefined : parseInt(price[0]),
-        date: today
-      }
+  switch (eventType) {
+    case "follow":
+      await followHandeler(event);
+      break;
+    case "message":
+      await messageHandeler(event);
+      break;
+    default:
+      console.log(`Unsupported event type: ${eventType}`);
+  }
+}
 
-      //push item into database and keep tract item
-      const add = await addItem(addItemConfig);
-      const itemId = add.data.id.replace(/-/g, "");
+const followHandeler = async (event) => {
+  const userId = event.source.userId;
+  const isVerified = await verifyUser(userId);
 
-      //get all list from database
-      let tags = await getAllTag();
-
-      //send richtext menu to select tag
-      const response = await client.replyMessage(
-        event.replyToken,
-        sendSelectTags(itemId, tags, detail == null ? false : true)
-      );
-    } else if (event.message.text == "รายจ่ายวันนี้") {
-      console.log("Message >>> รายจ่ายวันนี้");
-      const todayList = await getTodayItems();
-      console.log(today);
-      const response = await client.replyMessage(
-        event.replyToken,
-        message(todayExpense(todayList))
-      );
-      console.log("Log >>> send message success")
-    } else if (event.message.text == "เงินที่ใช้ได้") {
-      console.log("Message >>> เงินที่ใช้ได้");
-
-      const items = await getAllItems();
-      const netAsset = items
-        .map(item => item.properties["รับ-จ่าย"].number)
-        .reduce((pre, next) => pre + next, 0);
-
-      const daysInMonth = dayjs(today).daysInMonth();
-      const date = dayjs().get('date');
-
-      const response = await client.replyMessage(
-        event.replyToken,
-        message(`เงินที่เหลือเดือนนี้ ${netAsset}\nใช้ได้อีก ${daysInMonth-date} วัน\nเฉลี่ยใช้ได้ ${Math.floor(netAsset/(daysInMonth-date))} บาท/วัน`)
-      );
-      console.log("Log >>> send message success")
-    } else {
-      console.log("Log >>> Not match message")
-    }
-  } else if (event.type == 'postback') {
-    //collecting data
-    const data = JSON.parse(event.postback.data)
-    const {input, pageId:itemId} = data;
-    console.log("Post back >>> " + input)
-
-    //property to update
-    const update_config = {
-      pageId: itemId
-    }
-
-    //capture input type
-    switch (input) {
-      case "add_list":
-        update_config.list = data.list;
-        if(!data.has_detail) update_config.detail = data.list;
-        //update list
-        await updateItem(update_config);
-        console.log("Log >>> add list successs")
-
-        //get today items
-        const todayItems = await getTodayItems();
-        const incomeExpenseList = extractNetvaule(todayItems)
-
-        //calculate net
-        const todayNet = incomeExpenseList.reduce((sum, pre) => sum + pre, 0);
-        console.log(`Log >>> รวมรายรับรายจ่ายวันนี้ = ${todayNet}`)
-
-        //send net asset
-        await client.replyMessage(
-          event.replyToken,
-          message(`รวมรายรับรายจ่ายวันนี้\n ${todayNet.toString()} บาท`)
-        );
-
-        break;
-      default:
-        break;
-    }
+  if (!isVerified) {
+    await addUserDocument(userId);
+    return;
   }
 
-  return event;
-}
+  console.log("You have already verified");
+  return;
+};
+
+const messageHandeler = async (event) => {
+  const userId = event.source.userId;
+  const message = event.message.text;
+  const isVerified = await verifyUser(userId);
+
+  if (!isVerified) {
+    //await sendMessages(event.replyToken, messages);
+    return;
+  }
+
+  const isAccountingObject = parseAccoutingMessage(message);
+
+  if(isAccountingObject) {
+    await addToList({...isAccountingObject, user_id: userId})
+
+    const today = moment().format("DD/MM/YYYY");
+
+    const data = await filterDataByDateAndUserId(today, userId);
+    const messages = [todayAccoutingList(data)];
+
+    await sendMessages(event.replyToken, messages);
+    return;
+  }
+};
+
+// Helper function
+
+const sendMessages = async (replyToken, messages) => {
+
+  const response = await client.replyMessage(
+    replyToken,
+    messages
+  );
+};
+
+const parseAccoutingMessage = (message) => {
+  // Split message text into lines
+  const lines = message.split("\n");
+
+  // Extract relevant information from lines
+  const date =
+    lines[0] && lines[0].startsWith("วันที่ ") ? lines[0].split(" ")[1] : null;
+  const tag =
+    lines[1] && lines[1].startsWith("รายการ ")
+      ? lines[1].split(" ")[1]
+      : null;
+  const detail =
+    lines[2] && lines[2].startsWith("รายละเอียด ") ? lines[2].split(" ")[1] : null;
+  const price =
+    lines[3] && lines[3].startsWith("ราคา ")
+      ? parseInt(lines[3].split(" ")[1])
+      : null;
+
+  // Check if extracted information is valid
+  if (!date || !detail || !tag || !price) {
+    console.log(`Normal text: ${message}`);
+    return null;
+  }
+
+  // Create data object
+  const data = {
+    date: date,
+    detail: detail,
+    tag: tag,
+    price: price.toString(),
+  };
+
+  // Do something with the data object (e.g. store it in a database)
+  return data;
+};
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ message-object.js
@@ -0,0 +1,115 @@
+export const todayAccoutingList = (data) => {
+  
+  function calculateTotalPrice(data) {
+    let totalPrice = 0;
+
+    data.forEach((item) => {
+      totalPrice += parseInt(item.price);
+    });
+
+    return totalPrice;
+  }
+
+  return {
+    type: "flex",
+    altText: "เพิ่มรายจ่ายใหม่",
+    contents: {
+      type: "bubble",
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: `รายการวันที่ ${data.date}`,
+            weight: "bold",
+            size: "md",
+          },
+          {
+            type: "separator",
+            margin: "md",
+          },
+          {
+            type: "box",
+            layout: "vertical",
+            margin: "md",
+            contents: [
+              {
+                type: "box",
+                layout: "horizontal",
+                contents: [
+                  {
+                    type: "text",
+                    text: "รายละเอียด",
+                    size: "sm",
+                    weight: "bold",
+                    flex: 3,
+                  },
+                  {
+                    type: "text",
+                    text: "ราคา",
+                    size: "sm",
+                    weight: "bold",
+                    align: "end",
+                    flex: 2,
+                  },
+                ],
+              },
+              {
+                type: "separator",
+                margin: "md",
+              },
+              // Iterate through the data array and generate a table row for each entry
+              ...data.list.map((expense) => ({
+                type: "box",
+                layout: "horizontal",
+                margin: "md",
+                contents: [
+                  {
+                    type: "text",
+                    text: expense.detail,
+                    size: "sm",
+                    flex: 3,
+                  },
+                  {
+                    type: "text",
+                    text: expense.price,
+                    size: "sm",
+                    align: "end",
+                    flex: 2,
+                  },
+                ],
+              })),
+              {
+                type: "separator",
+                margin: "md",
+              },
+              {
+                type: "box",
+                layout: "horizontal",
+                margin: "md",
+                contents: [
+                  {
+                    type: "text",
+                    text: "รวม",
+                    size: "sm",
+                    weight: "bold",
+                    flex: 3,
+                  },
+                  {
+                    type: "text",
+                    text: `${calculateTotalPrice(data.list).toString()} บาท`,
+                    size: "sm",
+                    weight: "bold",
+                    align: "end",
+                    flex: 2,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  };
+};
